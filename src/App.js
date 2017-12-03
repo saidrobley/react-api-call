80e116492a69b83a1cfde33f3c7d4dffaf989e31
Said Robley <saidrobley@gmail.com>
2017-12-02 18:58:57 -0700

    adding tables for displaying the data

--- src/App.js
+++ src/App.js
@@ -3,19 +3,73 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            data: [],
+           // title: {}
+        }
+    }
+    componentDidMount() {
+
+        fetch('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=309174531d4b4343b1e1a793eb338e9f')
+            .then((Response)=>Response.json())
+            .then((findresponse)=>{
+                console.log(findresponse.articles)
+
+                // console.log(findresponse.data)
+                this.setState({
+                    data:findresponse.articles,
+                 //   title:findresponse.description,
+                    //  data:findresponse.data,
+                    //  data:findresponse,
+                })
+            })
+
+    }
+    render() {
+
+        return(
+
+
+            <div>
+
+                {
+
+
+                    this.state.data.map((dynamicData, key) =>
+                        <table className={"pretty-table"}>
+                            <tr>
+                                <th>Author</th>
+                                <th colSpan={"2"}>Title</th>
+                            </tr>
+                            <tr></tr>
+                            <tr>
+                                <td>Author: {dynamicData.author}</td>
+                                <td>Title: {dynamicData.title}</td>
+
+                            </tr>
+                        </table>
+
+                    )
+                }
+            </div>
+        )
+    }
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
